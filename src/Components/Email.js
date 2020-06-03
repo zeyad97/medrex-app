b0bd8f1f77444ec3e4a75462bcb3ded0b7808dde
RamlahAziz <ramlah.aziz2012@gmail.com>
2020-06-03 23:33:50 +0500

    Email js committed with parameters needed

--- src/Components/Email.js
+++ src/Components/Email.js
@@ -4,12 +4,44 @@ import emailjs from 'emailjs-com';
 // this class is not being used anywhere, the function sendemail and the button have both been 
 // copied into PatientView.js. This class is for reference purpose when properly integrating 
 class email extends React.Component {
-    
-    sendEmail() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            doctor_email:this.props.docId,
+            mrn:this.props.m,
+            doctor_name:this.props.docId,
+            patient_name:this.props.docId
+        }
+    }
+
+    sendGrantAccessEmail() {
     
         var template_params = {
-            "patient_email": "ramlah.aziz2012@gmail.com",
-            "doctor": "Ramlah"
+            //"doctor_email":{this.state.doctor_email},
+            //"mrn":{this.state.mrn},
+            //"doctor_name":{this.state.mrn},
+            //"patient_name":{this.state.mrn}
+        }
+            
+        var service_id = "default_service";
+        var template_id = "medrex_grant_access";
+        // from emailjs website
+        var user_id = "user_ST7dldKNwGvYdwcYMbwjg";
+        emailjs.send(service_id, template_id, template_params, user_id)
+            .then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    }
+    
+    sendRequestAccessEmail() {
+    
+        var template_params = {
+            "patient_email":"",
+            "mrn":"",
+            "doctor_name":"",
+            "patient_name":""
         }
             
         var service_id = "default_service";
@@ -22,13 +54,13 @@ class email extends React.Component {
             }, function(error) {
                 console.log('FAILED...', error);
             });
-    } 
+    }
 
     render() {
         return (
             <button 
             className= "sendemail"
-            onClick={() => this.sendEmail()}
+            onClick={() => this.sendGrantAccessEmail()}
             >
                 {'Send Request Access Email'}
             </button>
